6648f37b2d5999b060a5c9a07f4c26b0f16ebccf
k-kimtaI <tezkim414@gmail.com>
2025-11-03 20:31:03 +0300

    (del)

--- client/web/src/services/keycloak.ts
+++
@@ -1,9 +0,0 @@
-import Keycloak from "keycloak-js";
-
-const keycloak = new Keycloak({
-  url: "http://localhost:8081/",
-  realm: "authsystem",
-  clientId: "authsystem-client",
-});
-
-export default keycloak;
